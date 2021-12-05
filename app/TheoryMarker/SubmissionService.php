83305ef934ca798a0b562f2db52c9acf8d5a4ac9
Godwin324 <gkalu30@gmail.com>
2021-12-05 20:18:25 +0100

    add checks for plural and singular forms as well as some popular articles and prepositions

--- app/TheoryMarker/SubmissionService.php
+++ app/TheoryMarker/SubmissionService.php
@@ -3,6 +3,9 @@
 namespace App\TheoryMarker;
 
 use App\Models\Question;
+use Doctrine\Inflector\Language;
+use Doctrine\Inflector\Inflector;
+use Doctrine\Inflector\InflectorFactory;
 
 class SubmissionService
 {
@@ -18,7 +21,8 @@ class SubmissionService
             $partAnswerOptions = explode(';', $partAnswer->answer);
 
             foreach ($partAnswerOptions as $option) {
-                if (stristr($answer, trim($option))) {
+
+                if ($this->findInAnswer($option, $answer)) {
                     $marksObtained += $partAnswer->mark;
                     break;
                 }
@@ -29,4 +33,51 @@ class SubmissionService
         return ['mark' => $marksObtained, 'total' => $question->marks_obtainable];
     }
 
+    public function findInAnswer(string $keyPhrase, string $answer): bool
+    {
+        $keyPhrase = trim($keyPhrase);
+        $foundDirectly = stristr($answer, $keyPhrase);
+
+        $inflector = InflectorFactory::createForLanguage(Language::ENGLISH)->build();
+
+        $foundAlternative = false;
+        $keyPhraseArray = explode(' ', $keyPhrase);
+        $wordCount = count($keyPhraseArray);
+        if ($wordCount > 1) {
+            //adds articles 'a', 'an', 'the', 'some' and 'any' to two word answers
+            $lastWord = $keyPhraseArray[$wordCount - 1];
+            $alternatives = [];
+            $articles = ['a', 'an', 'the', 'some', 'any', 'on'];
+
+            foreach ($articles as $article) {
+                $keyPhraseArray[$wordCount - 1] = $article;
+                $keyPhraseArray[$wordCount] = $inflector->singularize($lastWord);
+                $alternatives[] = implode(' ', $keyPhraseArray);
+
+                $keyPhraseArray[$wordCount] = $inflector->pluralize($lastWord);
+                $alternatives[] = implode(' ', $keyPhraseArray);
+            }
+            foreach ($alternatives as $alternative) {
+                $foundAlternative = stristr($answer, $alternative);
+                if ($foundAlternative) {
+                    break;
+                }
+            }
+
+        } else {
+            //check for singular and plural forms of words
+
+            $alternative = $inflector->pluralize($keyPhrase);
+            $foundAlternative = stristr($answer, $alternative);
+
+            if (!$foundAlternative) {
+                //in any case the word was already plural
+                $alternative = $inflector->singularize($keyPhrase);
+                $foundAlternative = stristr($answer, $alternative);
+            }
+        }
+
+
+        return $foundDirectly || $foundAlternative;
+    }
 }
